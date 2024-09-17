d3568d2868c393bd95011fd6df88edf423852f39
lucianossouza <luciano.illustrador@hotmail.com>
2024-09-17 11:12:47 -0300

    ajustment style and index

--- dados.js
+++ dados.js
@@ -4,7 +4,7 @@ let dados = [
     descricao: "As raposas são animais mamíferos e onívoros pertencentes à família Canidae. São vulpídeos de porte médio, caracterizados por um focinho comprido e uma cauda longa e peluda.Também apresentam como particularidade suas pupilas ovais, semelhantes às pupilas verticais dos felídeos. De cerca de 40 espécies reconhecidas como raposas, somente 12 pertencem ao gênero Vulpes das raposas verdadeiras, do qual a raposa vermelha é a mais comum.Também apresentam como particularidade suas pupilas ovais, semelhantes às pupilas verticais dos felídeos.",
     link: "https://pt.wikipedia.org/wiki/Raposa",
     tags: "Rapoza mamiferus mamiferu mamiferos",
-    imagem: "raposa.jpg" // Adicione o nome da imagem aqui
+    imagem: "fox.jpeg" // Adicione o nome da imagem aqui
   },
   {
     titulo: "Esquilo",
@@ -18,21 +18,21 @@ let dados = [
     descricao: "Os ursos são mamíferos de grande porte que habitam diversos continentes, desde as tundras árticas até as florestas tropicais. Apesar de sua aparência robusta, são animais bastante ágeis e adaptados a diferentes ambientes. A família dos Ursidae abrange uma variedade de espécies, cada uma com características únicas, como o urso polar, conhecido por sua pelagem branca, e o panda gigante, famoso por sua dieta à base de bambu.",
     link: "https://pt.wikipedia.org/wiki/Urso",
     tags: "Urço mamiferus mamiferu mamiferos",
-    imagem: "urso.jpg"
+    imagem: "bear.jpeg"
   },
   {
     titulo: "Leão",
     descricao: "O leão, conhecido como o rei da selva, é um felino social que vive em grupos chamados de leões. Os machos, facilmente reconhecidos por suas juba, são responsáveis por proteger o território e o grupo. As fêmeas são as principais caçadoras e criadoras dos filhotes. Leões são animais poderosos e adaptados para a vida em savanas, onde caçam grandes herbívoros.",
     link: "https://pt.wikipedia.org/wiki/Le%C3%A3o",
     tags: "leão, felino, rei da selva, savana, juba, leao",
-    imagem: "leao.jpg"
+    imagem: "lion.jpeg"
   },
   {
     titulo: "Lobo",
     descricao: "O lobo é um canídeo conhecido por sua inteligência e vida em grupo. Vivem em matilhas, onde há uma hierarquia social bem definida. São animais adaptados para a caça, com uma audição e olfato aguçados. Os lobos são símbolos de lealdade e força em muitas culturas.",
     link: "https://pt.wikipedia.org/wiki/Lobo",
     tags: "lobo, canídeo, matilha, caça, natureza",
-    imagem: "lobo.jpg"
+    imagem: "wolf.jpeg"
   }
 ];
 
